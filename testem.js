b5fc7726c89d8fef9954fa344a08b7a60dee9e1e
Dan Freeman <dfreeman@salsify.com>
2016-12-21 11:30:11 -0500

    Fix reporting for Circle

--- testem.js
+++ testem.js
@@ -1,13 +1,15 @@
-/*jshint node:true*/
+/* eslint-env node */
+/* eslint quotes:0 quote-props:0 */
 module.exports = {
   "framework": "qunit",
   "test_page": "tests/index.html?hidepassed",
+  "report_file": "./tmp/test-output/browser/output.xml",
   "disable_watching": true,
   "launch_in_ci": [
-    "PhantomJS"
+    "PhantomJS",
   ],
   "launch_in_dev": [
     "PhantomJS",
-    "Chrome"
-  ]
+    "Chrome",
+  ],
 };
